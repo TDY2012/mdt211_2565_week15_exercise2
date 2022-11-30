965af9889ebe754f608100bc290548601a7a6b48
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-12-01 01:41:49 +0700

    feat: Added solution for week 15 exercise 2.

---
+++ BinaryTree.cs
@@ -0,0 +1,154 @@
+class BinaryTree<T> where T : struct
+{
+    private TreeNode<T> root = null;
+    private int length = 0;
+
+    public void Add(T value)
+    {
+        TreeNode<T> node = new TreeNode<T>(value);
+        if(this.root == null)
+        {
+            this.root = node;
+        }
+        else
+        {
+            Queue<TreeNode<T>> nodeQueue = new Queue<TreeNode<T>>();
+            nodeQueue.Push(this.root);
+
+            TreeNode<T> ptr;
+            while(nodeQueue.GetLength() > 0)
+            {
+                ptr = nodeQueue.Pop();
+                if(ptr.Left() == null)
+                {
+                    ptr.SetLeft(node);
+                    break;
+                }
+                else if(ptr.Right() == null)
+                {
+                    ptr.SetRight(node);
+                    break;
+                }
+                else
+                {
+                    nodeQueue.Push(ptr.Left());
+                    nodeQueue.Push(ptr.Right());
+                }
+            }
+        }
+        this.length++;
+    }
+
+    public void AddLeft(int index, T value)
+    {
+        TreeNode<T> node = new TreeNode<T>(value);
+        if(index == -1)
+        {
+            node.SetLeft(this.root);
+            this.root = node;
+        }
+        else
+        {
+            TreeNode<T> ptr = this.GetTreeNode(index);
+            node.SetLeft(ptr.Left());
+            ptr.SetLeft(node);
+        }
+        this.length++;
+    }
+
+    public void AddRight(int index, T value)
+    {
+        TreeNode<T> node = new TreeNode<T>(value);
+        if(index == -1)
+        {
+            node.SetLeft(this.root);
+            this.root = node;
+        }
+        else
+        {
+            TreeNode<T> ptr = this.GetTreeNode(index);
+            node.SetRight(ptr.Right());
+            ptr.SetRight(node);
+        }
+        this.length++;
+    }
+
+    public void Remove(int index)
+    {
+        if(index == 0)
+        {
+            TreeNode<T> ptr = this.root;
+            this.root = ptr.Left();
+            if(this.root.Right() != null)
+            {
+                this.root.SetLeft(this.root.Right());
+                this.root.SetRight(null);
+            }
+        }
+        else
+        {
+            TreeNode<T> previousPtr = this.GetTreeNode(index-1);
+            if(previousPtr.Left() != null)
+            {
+                TreeNode<T> ptr = previousPtr.Left();
+                previousPtr.SetLeft(ptr.Left());
+
+                if(ptr.Right() != null)
+                {
+                    ptr.Right().SetRight(ptr.Right());
+                }
+            }
+            else
+            {
+                TreeNode<T> ptr = previousPtr.Right();
+
+                if(ptr.Left() != null)
+                {
+                    previousPtr.SetRight(ptr.Left());
+                    ptr.Left().SetRight(ptr.Right());
+                }
+                else
+                {
+                    previousPtr.SetRight(ptr.Right());
+                }
+            }
+        }
+        this.length--;
+    }
+
+    public int GetLength()
+    {
+        return this.length;
+    }
+
+    public T Get(int index)
+    {
+        TreeNode<T> ptr = this.GetTreeNode(index);
+        return ptr.GetValue();
+    }
+
+    private TreeNode<T> GetTreeNode(int index)
+    {
+        int traverseStep = index;
+        return this.Traverse(this.root, ref traverseStep);
+    }
+
+    private TreeNode<T> Traverse(TreeNode<T> currentTreeNode, ref int traverseStep)
+    {
+        TreeNode<T> ptr = currentTreeNode;
+
+        if(traverseStep > 0 && currentTreeNode.Left() != null)
+        {
+            traverseStep--;
+            ptr = this.Traverse(currentTreeNode.Left(), ref traverseStep);
+        }
+
+        if(traverseStep > 0 && currentTreeNode.Right() != null)
+        {
+            traverseStep--;
+            ptr = this.Traverse(currentTreeNode.Right(), ref traverseStep);
+        }
+
+        return ptr;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BinaryTree<char> tree = new BinaryTree<char>();
+
+        tree.Add('A');
+        tree.Add('B');
+        tree.Add('C');
+        tree.Add('D');
+        tree.Add('E');
+
+        for(int i=0; i<tree.GetLength(); i++)
+        {
+            Console.WriteLine(tree.Get(i));
+        }
+    }
+}
